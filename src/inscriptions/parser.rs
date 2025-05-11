76e60727ad1d6fd52eaa46915853bf8b44cb597a
Dlamar <dlamardima@gmail.com>
2025-05-11 17:09:01 +0300

    add raw block parse

--- src/inscriptions/parser.rs
+++ src/inscriptions/parser.rs
@@ -1,35 +1,92 @@
-use super::*;
+use super::{
+    structs::{Part, Partials},
+    *,
+};
 
 pub struct ParseInscription<'a> {
     tx: &'a Transaction,
-    input_idx: usize,
-    inscription_idx: &'a mut u32,
+    input_index: u32,
     inputs_cum: &'a [u64],
+    partials: &'a Partials,
 }
 
 pub struct InitialIndexer;
 
 impl InitialIndexer {
+    fn load_partials(server: &Server, outpoints: Vec<OutPoint>) -> HashMap<OutPoint, Partials> {
+        server
+            .db
+            .outpoint_to_partials
+            .multi_get(outpoints.iter())
+            .into_iter()
+            .zip(outpoints)
+            .filter_map(|(partials, outpoint)| partials.map(|partials| (outpoint, partials)))
+            .collect()
+    }
+
+    fn load_inscription_outpoint_to_offsets(
+        server: &Server,
+        outpoints: Vec<OutPoint>,
+    ) -> HashMap<OutPoint, HashSet<u64>> {
+        server
+            .db
+            .outpoint_to_inscription_offsets
+            .multi_get(outpoints.iter())
+            .into_iter()
+            .zip(outpoints)
+            .filter_map(|(offsets, outpoint)| offsets.map(|offsets| (outpoint, offsets)))
+            .collect()
+    }
+
     fn parse_block(
+        server: &Server,
         height: u32,
         created: u32,
         txs: &[Transaction],
         prevouts: &HashMap<OutPoint, TxOut>,
         token_cache: &mut TokenCache,
     ) {
+        let take_multiple_input = if height as usize >= *JUBILEE_HEIGHT {
+            usize::MAX
+        } else {
+            1
+        };
+
         let mut transfers = vec![];
+
+        let coinbase_value = txs
+            .first()
+            .map(|coinbase| {
+                coinbase
+                    .output
+                    .iter()
+                    .map(|outpoint| outpoint.value)
+                    .sum::<u64>()
+            })
+            .unwrap_or_default();
+
+        // Hold inscription's partials from db and new in the block
+        let mut outpoint_to_partials =
+            Self::load_partials(server, prevouts.keys().cloned().collect());
+
+        // Hold inscription's partials to remove from db
+        let partials_to_remove: HashSet<_> = outpoint_to_partials.keys().cloned().collect();
+
+        let mut inscription_outpoint_to_offsets =
+            Self::load_inscription_outpoint_to_offsets(server, prevouts.keys().cloned().collect());
 
         for tx in txs {
             if tx.is_coin_base() {
                 continue;
             }
-            let mut inscription_idx = 0;
+
+            let mut inscription_index_in_tx = 0;
             let txid = tx.txid();
 
             let inputs_cum = InscriptionSearcher::calc_offsets(tx, prevouts)
                 .expect("failed to find all txos to calculate offsets");
 
-            for (idx, txin) in tx.input.iter().enumerate() {
+            for (input_index, txin) in tx.input.iter().enumerate() {
                 transfers.extend(
                     token_cache
                         .valid_transfers
@@ -45,51 +102,113 @@ impl InitialIndexer {
                         .map(|(k, (address, proto))| (*k, (*address, proto.clone()))),
                 );
 
-                for &(location, _) in &transfers {
-                    if location.outpoint != txin.previous_output {
-                        continue;
-                    }
-
-                    if let Ok((vout, _)) = InscriptionSearcher::get_output_index_by_input(
-                        inputs_cum.get(idx).map(|&x| x + location.offset),
-                        &tx.output,
-                    ) {
-                        if tx.output[vout as usize].script_pubkey.is_op_return() {
-                            token_cache.burned_transfer(location, txid, vout);
-                        } else {
-                            let owner =
-                                tx.output[vout as usize].script_pubkey.compute_script_hash();
-                            token_cache.transferred(location, owner, txid, vout);
-                        };
-                    } else {
-                        token_cache.transferred(
-                            location,
-                            prevouts
-                                .get(&txin.previous_output)
-                                .unwrap()
-                                .script_pubkey
-                                .compute_script_hash(),
-                            tx.txid(),
-                            0,
-                        );
+                // handle inscription moves
+                if let Some(inscription_offsets) =
+                    inscription_outpoint_to_offsets.remove(&txin.previous_output)
+                {
+                    for inscription_offset in inscription_offsets {
+                        let offset = inputs_cum.get(input_index).map(|x| *x + inscription_offset);
+                        match InscriptionSearcher::get_output_index_by_input(offset, &tx.output) {
+                            Ok((new_vout, new_offset)) => {
+                                inscription_outpoint_to_offsets
+                                    .entry(OutPoint {
+                                        txid,
+                                        vout: new_vout,
+                                    })
+                                    .or_default()
+                                    .insert(new_offset);
+                            }
+                            _ => todo!(), // TODO handle leak
+                        }
                     }
                 }
 
-                for inc in Self::parse_inscriptions(ParseInscription {
-                    tx,
-                    input_idx: idx,
-                    inscription_idx: &mut inscription_idx,
-                    inputs_cum: &inputs_cum,
-                }) {
-                    if inc.genesis.index == 0
-                        || height as usize >= *MULTIPLE_INPUT_BEL_20_ACTIVATION_HEIGHT
-                    {
-                        if let Some(proto) = token_cache.parse_token_action(&inc, height, created) {
+                // handle inscription creation
+                if input_index < take_multiple_input {
+                    let mut partials = outpoint_to_partials
+                        .remove(&txin.previous_output)
+                        .unwrap_or(Partials {
+                            genesis_txid: txid,
+                            inscription_index: 0,
+                            parts: vec![],
+                        });
+
+                    let part = if let Some(tapscript) = txin.witness.tapscript() {
+                        Part {
+                            is_tapscript: true,
+                            script_buffer: tapscript.to_bytes(),
+                        }
+                    } else {
+                        Part {
+                            is_tapscript: false,
+                            script_buffer: txin.script_sig.to_bytes(),
+                        }
+                    };
+
+                    partials.parts.push(part);
+
+                    let parsed_result = Self::parse_inscription(ParseInscription {
+                        tx,
+                        input_index: input_index as u32,
+                        inputs_cum: &inputs_cum,
+                        partials: &partials,
+                    });
+
+                    let inscription_templates = match parsed_result {
+                        ParsedInscriptionResult::None => continue,
+                        ParsedInscriptionResult::Partials => {
+                            if partials.genesis_txid == txid {
+                                partials.inscription_index = inscription_index_in_tx;
+                                inscription_index_in_tx += 1;
+                            }
+                            outpoint_to_partials.insert(txin.previous_output, partials);
+                            continue;
+                        }
+                        ParsedInscriptionResult::Single(mut inscription_template) => {
+                            if partials.genesis_txid == txid {
+                                inscription_template.genesis.index = inscription_index_in_tx;
+                                inscription_index_in_tx += 1;
+                            }
+                            vec![inscription_template]
+                        }
+                        ParsedInscriptionResult::Many(mut inscription_templates) => {
+                            if partials.genesis_txid == txid {
+                                inscription_templates
+                                    .iter_mut()
+                                    .for_each(|inscription_template| {
+                                        inscription_template.genesis.index =
+                                            inscription_index_in_tx;
+                                        inscription_index_in_tx += 1;
+                                    });
+                            }
+
+                            inscription_templates
+                        }
+                    };
+
+                    for inscription_template in inscription_templates {
+                        let offset_occupied = !inscription_outpoint_to_offsets
+                            .entry(inscription_template.location.outpoint)
+                            .or_default()
+                            .insert(inscription_template.location.offset); // return false if item already exist
+
+                        // skip inscription which was created into occupied offset
+                        if !inscription_template.leaked && offset_occupied {
+                            continue;
+                        }
+
+                        // handle token deploy|mint|transfer creation
+                        if let Some(proto) =
+                            token_cache.parse_token_action(&inscription_template, height, created)
+                        {
                             transfers.push((
-                                inc.location,
-                                (inc.owner, TransferProtoDB::from_proto(proto, height)),
-                            ))
-                        };
+                                inscription_template.location,
+                                (
+                                    inscription_template.owner,
+                                    TransferProtoDB::from_proto(proto, height),
+                                ),
+                            ));
+                        }
                     }
                 }
             }
@@ -204,6 +323,7 @@ impl InitialIndexer {
         );
 
         Self::parse_block(
+            &server,
             block_height,
             created,
             &block.txdata,
@@ -364,81 +484,99 @@ impl InitialIndexer {
         Ok(())
     }
 
-    fn parse_inscriptions(payload: ParseInscription) -> Vec<InscriptionTemplate> {
-        let mut result = vec![];
-
-        for inscription in Inscription::from_transaction(payload.tx, payload.input_idx) {
-            match inscription {
-                ParsedInscription::None => {}
-                ParsedInscription::Partial => {}
-                ParsedInscription::Complete(inscription) => {
-                    let genesis = {
-                        InscriptionId {
-                            txid: payload.tx.txid(),
-                            index: *payload.inscription_idx,
-                        }
-                    };
-
-                    *payload.inscription_idx += 1;
-
-                    let content_type = inscription.content_type().map(|x| x.to_owned());
-
-                    let pointer = inscription.pointer();
-
-                    let mut inc = InscriptionTemplate {
-                        content: inscription.into_body(),
-                        content_type,
-                        genesis,
-                        location: Location {
-                            offset: 0,
-                            outpoint: OutPoint {
-                                txid: payload.tx.txid(),
-                                vout: payload.input_idx as u32,
-                            },
-                        },
-                        owner: FullHash::ZERO,
-                        value: 0,
-                        leaked: false,
-                    };
-
-                    let Ok((mut vout, mut offset)) = InscriptionSearcher::get_output_index_by_input(
-                        payload.inputs_cum.get(payload.input_idx).copied(),
-                        &payload.tx.output,
-                    ) else {
-                        continue;
-                    };
-
-                    if let Ok((new_vout, new_offset)) =
-                        InscriptionSearcher::get_output_index_by_input(pointer, &payload.tx.output)
-                    {
-                        vout = new_vout;
-                        offset = new_offset;
-                    }
-
-                    let location: Location = Location {
-                        outpoint: OutPoint {
-                            txid: payload.tx.txid(),
-                            vout,
-                        },
-                        offset,
-                    };
-
-                    let tx_out = &payload.tx.output[vout as usize];
-
-                    if tx_out.script_pubkey.is_op_return() {
-                        inc.owner = *OP_RETURN_HASH;
-                    } else {
-                        inc.owner = tx_out.script_pubkey.compute_script_hash();
-                    }
-
-                    inc.location = location;
-                    inc.value = tx_out.value;
-
-                    result.push(inc);
-                }
+    fn convert_to_template(
+        payload: &ParseInscription,
+        inscription: Inscription,
+    ) -> InscriptionTemplate {
+        let genesis = {
+            InscriptionId {
+                txid: payload.partials.genesis_txid,
+                index: 0,
             }
+        };
+
+        let content_type = inscription.content_type().map(|x| x.to_owned());
+
+        let pointer = inscription.pointer();
+
+        let mut inscription_template = InscriptionTemplate {
+            content: inscription.into_body(),
+            content_type,
+            genesis,
+            location: Location {
+                offset: 0,
+                outpoint: OutPoint {
+                    txid: payload.tx.txid(),
+                    vout: payload.input_index,
+                },
+            },
+            owner: FullHash::ZERO,
+            value: 0,
+            leaked: false,
+        };
+
+        let Ok((mut vout, mut offset)) = InscriptionSearcher::get_output_index_by_input(
+            payload
+                .inputs_cum
+                .get(payload.input_index as usize)
+                .copied(),
+            &payload.tx.output,
+        ) else {
+            inscription_template.leaked = true;
+            return inscription_template;
+        };
+
+        if let Ok((new_vout, new_offset)) =
+            InscriptionSearcher::get_output_index_by_input(pointer, &payload.tx.output)
+        {
+            vout = new_vout;
+            offset = new_offset;
         }
 
-        result
+        let location: Location = Location {
+            outpoint: OutPoint {
+                txid: payload.tx.txid(),
+                vout,
+            },
+            offset,
+        };
+
+        let tx_out = &payload.tx.output[vout as usize];
+
+        if tx_out.script_pubkey.is_op_return() {
+            inscription_template.owner = *OP_RETURN_HASH;
+        } else {
+            inscription_template.owner = tx_out.script_pubkey.compute_script_hash();
+        }
+
+        inscription_template.location = location;
+        inscription_template.value = tx_out.value;
+
+        inscription_template
     }
+
+    fn parse_inscription(payload: ParseInscription) -> ParsedInscriptionResult {
+        let parsed = Inscription::from_parts(&payload.partials.parts, payload.input_index);
+
+        match parsed {
+            ParsedInscription::None => ParsedInscriptionResult::None,
+            ParsedInscription::Partial => ParsedInscriptionResult::Partials,
+            ParsedInscription::Single(inscription) => {
+                ParsedInscriptionResult::Single(Self::convert_to_template(&payload, inscription))
+            }
+            ParsedInscription::Many(inscriptions) => ParsedInscriptionResult::Many(
+                inscriptions
+                    .into_iter()
+                    .map(|inscription| Self::convert_to_template(&payload, inscription))
+                    .collect(),
+            ),
+        }
+    }
+}
+
+pub enum ParsedInscriptionResult {
+    None,
+    Partials,
+    Single(InscriptionTemplate),
+    Many(Vec<InscriptionTemplate>),
 }
